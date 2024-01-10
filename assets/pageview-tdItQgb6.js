97a1fba39b9910998ffda75c0dd0d574e5ef3e8f
QuadnucYard <50077758+QuadnucYard@users.noreply.github.com>
2024-01-10 02:34:16 +0000

    Deploying to gh-pages from @ QuadnucYard/my-blog-private@fd654d8be7f206a1168c67dc76d9604ef51ddf42 🚀

--- assets/pageview-tdItQgb6.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./app-gR0I4E4L.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://quadnucyard-blog.vercel.app"};const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-gR0I4E4L.js").then(r=>r.M),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}

e37a71332b8c3a290dc551b3932317eec9995c37
QuadnucYard <50077758+QuadnucYard@users.noreply.github.com>
2024-01-11 06:46:13 +0000

    Deploying to gh-pages from @ QuadnucYard/my-blog-private@a9812e4be78b81bf417d502ba2fdd14f3d9d71dc 🚀

--- assets/pageview-eqZxcRB-.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./app-SLluY_-8.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://waline.quadnucyard.top"};const i=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-SLluY_-8.js").then(r=>r.M),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{i as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
